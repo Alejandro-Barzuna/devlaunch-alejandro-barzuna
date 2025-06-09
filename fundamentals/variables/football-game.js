cc11e21433b17b453b0adf88d528322093a0b90f
Alejandro Barzuna <0barzu@gmail.com>
2025-06-09 11:51:46 -0600

    chore(variables): add football-game exercise

---
+++ fundamentals/variables/football-game.js
@@ -0,0 +1,19 @@
+/*
+El evento es parte de las Eliminatorias Sudamericanas para la Copa del Mundo.
+En un partido celebrado el 22/11/23 a las 20:30, el equipo local Argentina
+se enfrentó al equipo visitante Brasil. El resultado final fue 1-0,
+indicando a Argentina como el Equipo ganador.
+*/
+
+const eventName = 'Eliminatorias Sudamericanas'
+
+const date = '22/11/23'
+const time = '20:30'
+
+const localTeam = 'Argentina'
+const awayTeam = 'Brazil'
+
+console.log(eventName)
+console.log(date)
+console.log(time)
+console.log(localTeam,awayTeam)
