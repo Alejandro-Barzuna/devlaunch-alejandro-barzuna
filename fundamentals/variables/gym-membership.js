f57e790d79ea6dcd69d216b01748ff98a854e0f7
Alejandro Barzuna <0barzu@gmail.com>
2025-06-09 16:07:02 -0600

    chore(variables): add gym-membership exercise

---
+++ fundamentals/variables/gym-membership.js
@@ -0,0 +1,26 @@
+/*
+El Elite Fitness es un gimnasio boutique ubicado en Palo Alto, California,
+que ofrece membresías por $150 al mes.
+Opera de 6 AM a 10 PM, emplea a 10 entrenadores certificados
+y cuenta con comodidades como sauna, piscina y clases grupales.
+*/
+
+const gymName = 'Elite Fitness'
+const gymLocation = 'Palo Alto, California'
+const price = '$150'
+
+const openingTime = '6 AM'
+const closingTime = '10 PM'
+const amountOfTrainers = 10
+
+const amenity1 = 'Sauna'
+const amenity2 = 'piscina'
+const amenity3 = 'clases grupales'
+
+console.log(`
+    Nombre del gimnasio: ${gymName}
+    Ubicación: ${gymLocation}
+    Horario: ${openingTime} a ${closingTime}
+    Entrenadores certificados: ${amountOfTrainers}
+    Beneficios: ${amenity1}, ${amenity2} y ${amenity3}
+    `)
